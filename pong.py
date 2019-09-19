4af4e3dcfcca537a538abf33cdf76ccf488d092a
vasilii <vkirillov@bmail.ru>
2019-09-19 20:08:18 +0400

    ping pong game using turtle module. init commit

---
+++ pong.py
@@ -0,0 +1,44 @@
+# ping pong game using oob libraries
+import turtle
+
+window = turtle.Screen()
+window.title("Ping pong python game")
+window.bgcolor("#451261")
+window.setup(width=800, height=600)
+window.tracer(0) #stops window from repainting. it will speed up game
+
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0) #speed of repaining
+paddle_a.shape("square") #square cyrcle triangle
+paddle_a.color("#c499f3")
+paddle_a.penup() #do not drow line while moving
+paddle_a.goto(-350, 0)
+# default size 20px x 20 px
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+
+
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0) #speed of repaining
+paddle_b.shape("square") #square cyrcle triangle # Ball
+paddle_b.color("#c499f3")
+paddle_b.penup() #do not drow line while moving
+paddle_b.goto(350, 0)
+paddle_b.shapesize(stretch_wid=5, stretch_len=1) 
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square") #square cyrcle triangle # Ball
+ball.color("#c499f3")
+ball.penup() #do not drow line while moving
+ball.goto(0, 0)
+
+
+# Main game loop
+while True:
+    window.update()
+
